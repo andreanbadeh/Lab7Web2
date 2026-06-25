e3b291d3fccb670867aeea4de81b8efc68962fa9
andreanputraarya <168192819+andreanbadeh@users.noreply.github.com>
2026-06-25 14:13:24 +0700

    Add files via upload

---
+++ lab8_vuejs/assets/js/components/About.js
@@ -0,0 +1,34 @@
+const About = {
+    template: `
+        <div class="home-container">
+
+            <h2>Profil Mahasiswa Pertengahan</h2>
+
+            <img
+                src="https://ui-avatars.com/api/?name=Andrean+Putra"
+                width="150"
+                style="border-radius:50%;margin-bottom:15px;"
+            >
+
+            <table>
+
+                <tr>
+                    <td>Nama</td>
+                    <td>: Andrean Putra Arya</td>
+                </tr>
+
+                <tr>
+                    <td>NIM</td>
+                    <td>: 312410341</td>
+                </tr>
+
+                <tr>
+                    <td>Kelas</td>
+                    <td>: I241E</td>
+                </tr>
+
+            </table>
+
+        </div>
+    `
+};
